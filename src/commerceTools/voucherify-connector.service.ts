0da1fc5f46e08c909c79d897a8f2d005a1db40df
Tomek Lochmanczyk <tomasz.lochmanczyk@rspective.com>
2022-05-09 11:22:44 +0200

    remove mock controllers

--- src/commerceTools/voucherify-connector.service.ts
+++ src/commerceTools/voucherify-connector.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { VoucherifyServerSide } from '@voucherify/sdk' ;
+import { VoucherifyServerSide } from '@voucherify/sdk';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class VoucherifyConnectorService {
-    constructor(private configService: ConfigService) {}
-    private readonly applicationId: string = this.configService.get<string>('VOUCHERIFY_APP_ID');
-    private readonly secretKey: string = this.configService.get<string>('VOUCHERIFY_SECRET_KEY');
+  constructor(private configService: ConfigService) {}
+  private readonly applicationId: string =
+    this.configService.get<string>('VOUCHERIFY_APP_ID');
+  private readonly secretKey: string = this.configService.get<string>(
+    'VOUCHERIFY_SECRET_KEY',
+  );
 
-    getClient(): ReturnType<typeof VoucherifyServerSide> {
-        return VoucherifyServerSide({
-            applicationId: this.applicationId,
-            secretKey: this.secretKey,
-        })
-    }
+  getClient(): ReturnType<typeof VoucherifyServerSide> {
+    return VoucherifyServerSide({
+      applicationId: this.applicationId,
+      secretKey: this.secretKey,
+    });
+  }
 }
