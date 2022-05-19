4345b0f12bb3a134d3005340b81acd217d88f12f
Marcin Ślęzak <35666945+marcin-slezak@users.noreply.github.com>
2022-05-19 16:32:24 +0200

    attach right port on heroku

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { JsonLoggerService } from 'json-logger-service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(new JsonLoggerService('NestServer'));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
