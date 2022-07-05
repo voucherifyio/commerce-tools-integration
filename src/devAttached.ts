0362dc0c3967661ba3b40f09b7cd0835b57e115e
jkaliszuk <jakub.kaliszuk@rspective.pl>
2022-07-05 12:10:34 +0200

    Added CUSTOM_NGROK_BIN_PATH to env and usage in devAttached.ts.

--- src/devAttached.ts
+++ src/devAttached.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { JsonLoggerService } from 'json-logger-service';
 import { RegisterService as RegisterApiEstension } from './api-extension/register.service';
 import * as ngrok from 'ngrok';
+const { join } = require("path");
 
 async function bootstrap() {
   const logger = new JsonLoggerService('NestServer');
   logger.log('Launching ngrok service');
-  const url = await ngrok.connect(3000);
+  const customNgrokBinPath = process.env.CUSTOM_NGROK_BIN_PATH;
+  const url = await ngrok.connect({addr: 3000, binPath: customNgrokBinPath ? (() => join(`${customNgrokBinPath}`)) : undefined});
   logger.log(`Application available ${url}`);
   const app = await NestFactory.create(AppModule);
   app.useLogger(logger);
@@ -20,4 +22,4 @@ async function bootstrap() {
     logger.error({ msg: 'Could not register Api Extension in Commerce Tools' });
   }
 }
-bootstrap();
+bootstrap()
