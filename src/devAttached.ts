6d0683d1792cdbdb126b6963ba535990152b98ca
jkaliszuk <jakub.kaliszuk@rspective.pl>
2022-07-05 12:10:56 +0200

    Change required to import

--- src/devAttached.ts
+++ src/devAttached.ts
@@ -3,13 +3,18 @@ import { AppModule } from './app.module';
 import { JsonLoggerService } from 'json-logger-service';
 import { RegisterService as RegisterApiEstension } from './api-extension/register.service';
 import * as ngrok from 'ngrok';
-const { join } = require("path");
+import { join } from 'path';
 
 async function bootstrap() {
   const logger = new JsonLoggerService('NestServer');
   logger.log('Launching ngrok service');
   const customNgrokBinPath = process.env.CUSTOM_NGROK_BIN_PATH;
-  const url = await ngrok.connect({addr: 3000, binPath: customNgrokBinPath ? (() => join(`${customNgrokBinPath}`)) : undefined});
+  const url = await ngrok.connect({
+    addr: 3000,
+    binPath: customNgrokBinPath
+      ? () => join(`${customNgrokBinPath}`)
+      : undefined,
+  });
   logger.log(`Application available ${url}`);
   const app = await NestFactory.create(AppModule);
   app.useLogger(logger);
@@ -22,4 +27,4 @@ async function bootstrap() {
     logger.error({ msg: 'Could not register Api Extension in Commerce Tools' });
   }
 }
-bootstrap()
+bootstrap();
