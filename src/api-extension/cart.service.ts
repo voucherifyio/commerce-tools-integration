2104179213d14b47ad85b004bddce3e5a24ccea5
jkaliszuk <jakub.kaliszuk@rspective.pl>
2022-07-13 08:39:08 +0200

    First concept with fixing bug. Change maping applicableCoupons logic.

--- src/api-extension/cart.service.ts
+++ src/api-extension/cart.service.ts
@@ -582,9 +582,10 @@ export class CartService {
             code: coupon.id,
             status: 'APPLIED',
             value:
-              coupon.order?.total_applied_discount_amount ||
+              coupon.order?.total_applied_discount_amount ??
+              coupon.order?.total_discount_amount ??
               oldCouponsCodes.find((oldCoupon) => coupon.id === oldCoupon.code)
-                ?.value ||
+                ?.value ??
               0,
           } as Coupon),
       ),
