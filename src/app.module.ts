1baca16e3d32e65e13388aae8b7c8084e6746471
Marcin Ślęzak <35666945+marcin-slezak@users.noreply.github.com>
2022-07-15 15:40:44 +0200

    fix import for mkdirp

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { APP_PIPE } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import * as Joi from 'joi';
 import * as path from 'path';
-import * as mkdirp from 'mkdirp';
+import mkdirp from 'mkdirp';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -89,7 +89,6 @@ import { RequestJsonFileLogger } from './misc/request-json-file-logger';
             'Please provide value of DEBUG_STORE_REQUESTS_DIR env variable!',
           );
         }
-
         await mkdirp(path.join(process.cwd(), requestsDir));
         return new RequestJsonFileLogger(requestsDir);
       },
