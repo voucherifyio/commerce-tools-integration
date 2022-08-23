bc3247112ff7697bfbf671556cda5b60dcfecf71
jkaliszuk <jakub.kaliszuk@rspective.pl>
2022-08-23 08:28:55 +0200

    Revert branch.

--- src/api-extension/cart.service.ts
+++ src/api-extension/cart.service.ts
@@ -266,7 +266,7 @@ export class CartService {
     const actions = getCartActionBuilders(validateCouponsResult).flatMap(
       (builder) => builder(cart, validateCouponsResult),
     );
-
+    console.log('test');
     this.logger.debug(actions);
     return {
       status: true,
