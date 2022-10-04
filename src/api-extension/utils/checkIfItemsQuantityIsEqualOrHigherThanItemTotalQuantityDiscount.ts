207475e3922a48744547b6ba03d50e5452b6a365
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2022-10-04 10:23:28 +0200

    Update checkIfItemsQuantityIsEqualOrHigherThanItemTotalQuantityDiscount.ts

--- src/api-extension/utils/checkIfItemsQuantityIsEqualOrHigherThanItemTotalQuantityDiscount.ts
+++ src/api-extension/utils/checkIfItemsQuantityIsEqualOrHigherThanItemTotalQuantityDiscount.ts
@@ -4,23 +4,19 @@ export default function checkIfItemsQuantityIsEqualOrHigherThanItemTotalQuantity
   lineItems: LineItem[],
 ): boolean {
   return !!lineItems?.find((lineItem) => {
-    if (lineItem.custom?.fields?.applied_codes) {
-      const { quantity: itemQuantity } = lineItem;
-      const totalQuantityDiscount = (
-        lineItem.custom?.fields?.applied_codes ?? []
-      )
-        .map((code) => JSON.parse(code))
-        .filter((code) => code.type === 'UNIT')
-        .reduce((sum, codeObject) => {
-          if (codeObject.quantity) {
-            sum += codeObject.quantity;
-          }
-          return sum;
-        }, 0);
-      if (totalQuantityDiscount > itemQuantity) {
-        return true;
-      }
+    if (!lineItem.custom?.fields?.applied_codes?.length) {
+      return false;
     }
-    return false;
+    const { quantity: itemQuantity } = lineItem;
+    const totalQuantityDiscount = lineItem.custom?.fields?.applied_codes
+      .map((code) => JSON.parse(code))
+      .filter((code) => code.type === 'UNIT')
+      .reduce((sum, codeObject) => {
+        if (codeObject.quantity) {
+          sum += codeObject.quantity;
+        }
+        return sum;
+      }, 0);
+    return totalQuantityDiscount > itemQuantity;
   });
 }
