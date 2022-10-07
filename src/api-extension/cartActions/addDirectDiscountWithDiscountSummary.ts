650554b062579cacc19e12aca45859a57028ad7f
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2022-10-07 15:55:10 +0200

    Update addDirectDiscountWithDiscountSummary.ts

--- src/api-extension/cartActions/addDirectDiscountWithDiscountSummary.ts
+++ src/api-extension/cartActions/addDirectDiscountWithDiscountSummary.ts
@@ -6,27 +6,60 @@ export default function addDirectDiscountWithDiscountSummary(
   cart: Cart,
   validateCouponsResult: ValidateCouponsResult,
 ): CartActionSetDirectDiscounts[] {
-  const { totalDiscountAmount, applicableCoupons } = validateCouponsResult;
+  const { applicableCoupons } = validateCouponsResult;
   if (applicableCoupons.length === 0) return [];
   const { currencyCode } = cart.totalPrice;
 
-  return [
-    {
-      action: 'setDirectDiscounts',
-      discounts: [
-        {
-          target: { type: 'lineItems', predicate: 'true' },
+  const discounts = [];
+
+  applicableCoupons.forEach((coupon) => {
+    if (coupon.result.discount.effect === 'APPLY_TO_ORDER') {
+      discounts.push({
+        target: {
+          type: 'lineItems',
+          predicate: 'true',
+        },
+        value: {
+          type: 'absolute',
+          money: [
+            {
+              centAmount:
+                coupon?.order?.total_applied_discount_amount ||
+                coupon.order?.applied_discount_amount ||
+                0,
+              currencyCode,
+            },
+          ],
+        },
+      });
+    }
+    coupon.order.items.forEach((item) => {
+      if (item?.applied_discount_amount) {
+        discounts.push({
+          target: {
+            type: 'lineItems',
+            predicate: item?.sku?.source_id
+              ? `sku="${item?.sku?.source_id}"`
+              : 'true',
+          },
           value: {
             type: 'absolute',
             money: [
               {
-                centAmount: totalDiscountAmount || 0,
+                centAmount: item.applied_discount_amount,
                 currencyCode,
               },
             ],
           },
-        },
-      ],
+        });
+      }
+    });
+  });
+
+  return [
+    {
+      action: 'setDirectDiscounts',
+      discounts,
     },
   ];
 }
