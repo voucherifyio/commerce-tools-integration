9978218b050ae41f79a187a16f0c47c155dd9374
jkaliszuk <jakub.kaliszuk@rspective.pl>
2022-10-11 16:25:58 +0200

    Disabling cart level promotions.

--- src/api-extension/cart.service.ts
+++ src/api-extension/cart.service.ts
@@ -170,32 +170,19 @@ export class CartService {
         ? this.configService.get<number>('COMMERCE_TOOLS_COUPONS_LIMIT')
         : 5;
 
-    const promotions =
-      await this.voucherifyConnectorService.getAvailablePromotions(
+    const disableCartPromotion =
+      this.configService.get<string>('DISABLE_CART_PROMOTION') ?? 'false';
+
+    let promotions = [];
+    let availablePromotions = [];
+
+    if (disableCartPromotion.toLowerCase() === 'false') {
+      promotions = await this.voucherifyConnectorService.getAvailablePromotions(
         cart,
         this.productMapper.mapLineItems(cart.lineItems),
       );
-
-    const availablePromotions = promotions
-      .filter((promo) => {
-        if (!uniqCoupons.length) {
-          return true;
-        }
-
-        const codes = uniqCoupons
-          .filter((coupon) => coupon.status !== 'DELETED')
-          .map((coupon) => coupon.code);
-        return !codes.includes(promo.id);
-      })
-      .map((promo) => {
-        return {
-          status: 'AVAILABLE',
-          value: promo.discount_amount,
-          banner: promo.banner,
-          code: promo.id,
-          type: promo.object,
-        };
-      });
+      availablePromotions = await this.getPromotions(promotions, uniqCoupons);
+    }
 
     if (!uniqCoupons.length) {
       this.logger.debug({
@@ -278,7 +265,9 @@ export class CartService {
         );
     }
 
-    this.setBannerOnValidatedPromotions(validatedCoupons, promotions);
+    if (promotions.length) {
+      this.setBannerOnValidatedPromotions(validatedCoupons, promotions);
+    }
 
     const getCouponsByStatus = (status: StackableRedeemableResponseStatus) =>
       validatedCoupons.redeemables.filter(
@@ -337,6 +326,30 @@ export class CartService {
       taxCategory,
       couponsLimit,
     };
+  }
+
+  private async getPromotions(promotions, uniqCoupons: Coupon[]) {
+    const availablePromotions = promotions
+      .filter((promo) => {
+        if (!uniqCoupons.length) {
+          return true;
+        }
+
+        const codes = uniqCoupons
+          .filter((coupon) => coupon.status !== 'DELETED')
+          .map((coupon) => coupon.code);
+        return !codes.includes(promo.id);
+      })
+      .map((promo) => {
+        return {
+          status: 'AVAILABLE',
+          value: promo.discount_amount,
+          banner: promo.banner,
+          code: promo.id,
+          type: promo.object,
+        };
+      });
+    return availablePromotions;
   }
 
   private setBannerOnValidatedPromotions(
