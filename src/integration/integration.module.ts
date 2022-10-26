19d29a15c4c4ebd09d1d6562902c4dccee90a94b
Marcin Slezak <marcin.slezak@voucherify.io>
2022-10-26 12:41:16 +0200

    WIP

--- src/integration/integration.module.ts
+++ src/integration/integration.module.ts
@@ -1,14 +1,14 @@
 import { Module, Logger } from '@nestjs/common';
 import { CommercetoolsService } from './store/commercetools/commercetools.service';
 import { CommercetoolsController } from './store/commercetools/commercetools.controller';
-import { VoucherifyService } from './voucherify.service';
+import { VoucherifyConnectorService } from './voucherify.service';
 import { IntegrationService } from './integration.service';
 
 @Module({
   controllers: [CommercetoolsController],
   providers: [
     CommercetoolsService,
-    VoucherifyService,
+    VoucherifyConnectorService,
     IntegrationService,
     Logger,
   ],
